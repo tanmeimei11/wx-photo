5a92a9a0e4b34f5a66e18675dd254b922fa6d6c2
senzai <xussen@xusensens-MacBook-Pro.local>
2018-01-08 19:27:35 +0800

    index

---
+++ dist/components/index/shareOrCreateGroup.js
@@ -0,0 +1,49 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _wepy = require('./../../npm/wepy/lib/wepy.js');
+
+var _wepy2 = _interopRequireDefault(_wepy);
+
+var _common = require('./../../utils/common.js');
+
+var _api = require('./../../utils/api.js');
+
+var _login = require('./../../utils/login.js');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ShareOrCreateGroupClass = function (_wepy$component) {
+  _inherits(ShareOrCreateGroupClass, _wepy$component);
+
+  function ShareOrCreateGroupClass() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, ShareOrCreateGroupClass);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = ShareOrCreateGroupClass.__proto__ || Object.getPrototypeOf(ShareOrCreateGroupClass)).call.apply(_ref, [this].concat(args))), _this), _this.data = {
+      images: []
+    }, _this.methods = {}, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  return ShareOrCreateGroupClass;
+}(_wepy2.default.component);
+
+exports.default = ShareOrCreateGroupClass;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNoYXJlT3JDcmVhdGVHcm91cC5qcyJdLCJuYW1lcyI6WyJTaGFyZU9yQ3JlYXRlR3JvdXBDbGFzcyIsImRhdGEiLCJpbWFnZXMiLCJtZXRob2RzIiwiY29tcG9uZW50Il0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQ0E7Ozs7QUFDQTs7QUFDQTs7QUFDQTs7Ozs7Ozs7OztJQUNxQkEsdUI7Ozs7Ozs7Ozs7Ozs7O3dOQUNuQkMsSSxHQUFPO0FBQ0xDLGNBQVE7QUFESCxLLFFBR1BDLE8sR0FBVSxFOzs7O0VBSnlDLGVBQUtDLFM7O2tCQUFyQ0osdUIiLCJmaWxlIjoic2hhcmVPckNyZWF0ZUdyb3VwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5pbXBvcnQgd2VweSBmcm9tICd3ZXB5JztcbmltcG9ydCB7IHd4UHJvbWlzaWZ5IH0gZnJvbSAnLi4vLi4vdXRpbHMvY29tbW9uLmpzJztcbmltcG9ydCB7IHVwbG9hZEltYWdlVG9RaW5pdSB9IGZyb20gJy4uLy4uL3V0aWxzL2FwaS5qcyc7XG5pbXBvcnQgeyByZXF1ZXN0IH0gZnJvbSAnLi4vLi4vdXRpbHMvbG9naW4uanMnO1xuZXhwb3J0IGRlZmF1bHQgY2xhc3MgU2hhcmVPckNyZWF0ZUdyb3VwQ2xhc3MgZXh0ZW5kcyB3ZXB5LmNvbXBvbmVudCB7XG4gIGRhdGEgPSB7XG4gICAgaW1hZ2VzOiBbXVxuICB9O1xuICBtZXRob2RzID0ge1xuXG4gIH07XG4gIFxuXG5cbn1cbiJdfQ==
