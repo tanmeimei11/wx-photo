36ddbaa789b27e490230905cf17a2726aa084300
xiaofu <xiaofu@in66.com>
2018-01-09 23:25:38 +0800

    set

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ module.exports = {
   DOMAIN: 'http://qaactivity.in66.com',
   // DOMAIN: 'http://10.10.106.127:30929',
   // DOMAIN: 'https://activity.in66.com',
-  isMock: true,
+  isMock: false,
   isTrack: false,
   qnTokenUrl: 'https://www.in66.com/promo/commonapi/qiniutoken',
   // qnUploadUrl: "http://up.qiniu.com",
